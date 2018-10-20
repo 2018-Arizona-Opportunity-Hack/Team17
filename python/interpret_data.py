6cb35ce72f477bae9639172b263b15ff97328cfd
Suhas Vittal <suhaskvittal@gmail.com>
2018-10-20 13:27:42 -0700

    Added python and js folders. Began interpret data script.

---
+++ python/interpret_data.py
@@ -0,0 +1,9 @@
+
+
+def interpret(R, T):
+    """
+        R is a dictionary of relational matrices, such that R[(m, n)] corresponds to the
+        relational matrix between node m and n.
+        T is a dictionary of an array of constraint matrices, such that T[(n, n)]
+        corresponds to the array of constraint matrices for node n.
+    """
