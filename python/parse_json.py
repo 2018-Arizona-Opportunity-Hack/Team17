f2fac2c58a21fe8033d7933ac74d4eeec85920f4
Evan Hao <hao.amazing@gmail.com>
2018-10-20 16:10:35 -0700

    added parser from json API into python dictionaries for further analysis

---
+++ python/parse_json.py
@@ -0,0 +1,24 @@
+import numpy as np
+import json
+import requests
+
+loaded_json = json.loads(requests.get("API").json())
+
+
+def constraint(constraint_key):
+    """First, the constraint function will create a dictionary,
+    and place the ID (volunteer)'s endorsement in a dictionary, while also counting
+    the number of volunteers."""
+    adict = {}
+    N = 0
+    for x in loaded_json:
+        adict[x['id']] = x[constraint_key]
+        N += 1
+    """Now, make a Numpy 2D array that places the value 0 if there are no endorsements,
+     and 1 if there are."""
+    T = np.zeros(N, N)
+    for i in range(N):
+        for j in range(N):
+            if j in adict[i]:
+                T[i,j] = 1
+    return T
