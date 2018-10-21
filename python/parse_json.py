ef939a0b7ce208aab35ac72c52d10337e6068a4e
Evan Hao <hao.amazing@gmail.com>
2018-10-20 17:20:10 -0700

    parse update

--- python/parse_json.py
+++ python/parse_json.py
@@ -9,10 +9,9 @@ def constraint(constraint_key):
     """First, the constraint function will create a dictionary,
     and place the ID (volunteer)'s endorsement in a dictionary, while also counting
     the number of volunteers."""
-    adict = {}
     N = 0
+    adict = constraint_dict(constraint_key)
     for x in loaded_json:
-        adict[x['id']] = x[constraint_key]
         N += 1
     """Now, make a Numpy 2D array that places the value 0 if there are no endorsements,
      and 1 if there are."""
@@ -23,12 +22,11 @@ def constraint(constraint_key):
                 T[i,j] = 1
     return T
 
-def relationship(relationship_key):
-    adict = {}
+def rel_vol_x(relationship_key):
     key_types = []
     N = 0
+    adict = constraint_dict(relationship_key)
     for x in loaded_json:
-        val = x[relationship_key]
         adict[x['id']] = val
         N+= 1
         if val not in key_types:
@@ -38,3 +36,20 @@ def relationship(relationship_key):
         j = key_types.index(adict[i])
         R[i, j] = 1
     return R
+
+def constraint_dict(constraint_key):
+    adict = {}
+    for x in loaded_json:
+        adict[x['id']] = x[constraint_key]
+    return adict
+
+def rel_vol_x_dict(relationship_key):
+    adict = {}
+    key_types = []
+    for x in loaded_json:
+        val = x[relationship_key]
+        adict[x['id']] = val
+    return adict
+
+if __name__ == "__main__":
+    return Merge(constraint_dict(constraint_key), rel_vol_x_dict(relationship_key))
