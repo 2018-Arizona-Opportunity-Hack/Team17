3c3bbd3cbb9036091f5e27fad692e87b15aa9898
Suhas Vittal <suhaskvittal@gmail.com>
2018-10-21 07:09:40 -0700

    Finished email handling middleware on app level.

--- uro/app.js
+++ uro/app.js
@@ -77,14 +77,122 @@ function sendEmail(sender, volunteer, event, template) {
   });
 }
 
-function openInbox(cb) {
-  imap.openBox('INBOX', true, cb);
+// get affirmative respondents to email
+function getAffirmativeRespondents(fromDate) {
+  affRespondents = []
+  imap.openBox('INBOX', true, (err, box) => {
+    if (err) throw err;
+    imap.search(['UNSEEN', ['SINCE', fromDate]], (err, results) => {
+      if (err) throw err;
+      var f = imap.fetch(results, {bodies: ['FROM', 'BODY']});
+      f.on('message', (msg, seqno) => {
+        console.log("MESSAGE NO: " + seqno);
+        var prefix = "(#" + seqno + ") ";
+        var email = "";
+        var isNegative = "";
+        msg.on('body', (stream, info) => {
+          console.log(prefix + body);
+          var buffer = "";
+          stream.on("data", (chunk) => {
+            buffer += chunk.toString('utf8');
+          });
+          stream.once("end", () => {
+            if (info.which == 'BODY') {
+              /* parse the message for negatives:
+                no
+                not or n't
+                un-
+                apologies
+              */
+              var regex = RegExp("n[o,\'](?!\')t?|una.*?|apolog.*?", "g");
+              isNegative = regex.test(inspect(Imap.parsedHeader(buffer));
+            } else {
+              email = inspect(Imap.parsedHeader(buffer));
+            }
+          });
+        });
+        msg.once('attributes', function(attrs) {
+          console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+        });
+        msg.once('end', () => {
+          affRespondents.push(email);
+          console.log(prefix + 'Finished');
+        });
+      });
+      f.once('error', function(err) {
+        console.log('Fetch error: ' + err);
+      });
+      f.once('end', function() {
+        console.log('Done fetching all messages!');
+        imap.end();
+      });
+    });
+  });
+
+  return affRespondents;
 }
 
-// timer call for checking if reminder date is nearing
+function retrieveObjectFromAttribute(attrName, attrValue, volunteers) {
+  for (var p in volunteers) {
+    if (p[attrName] == attrValue) {return p;}
+  }
+  return 0;
+}
+
+// emails clients to find new volunteers
+function cycleThroughParticipants(SENDER, startDate, step, targetDate, A, B, wholeList, volunteers) {
+  // A is the standing list of questionables, B is the list to choose from,
+  // wholeList is the list of all volunteers.
+  // startDate is the date to start cycling, step is the number of days to wait before
+  // cycling again, and num is the number of times to cycle.
+  for (var p in A) {
+    sendEmail(SENDER, p["email"], event, "reminder");
+  }
+
+  var date = startDate;
+  if (date > targetDate) {return;}
+  var job = schedule.scheduleJob(date, () => {
+    var resp = getAffirmativeRespondents(date - step);
+    // connect affirmative respondents back to uuids
+    var affUuids = []
+    for (var em in resp) {
+      affUuids.push(retrieveObjectFromAttribute("email", em, volunteers)["UUID"]);
+    }
+
+    var newList = []
+    // cross-reference and update
+    for (var p in wholeList) {
+      if (affUuids.includes(p) || !A.includes(p)) {
+        newList.push(p);
+      }
+    }
+    var N = original.length - newList.length;
+    // first send emails to top N volunteers on alt
+    var C = []
+    var k = 0;
+    for (var p in B) {
+      if (k < N) {
+        sendEmail(SENDER, p["email"], event, "request");
+      } else {
+        C.append(p);
+      }
+    }
+
+    // update event on firebase
+    var id = event["event_id"];
+    var newKey = database.ref("/events/" + id + "/list").push().key;
+    var update = {};
+    update["/list/" + newKey] = newList;
+    firebase.database().ref().update(update);
+
+    cycleThroughParticipants(SENDER, date + step, targetDate, B, C, wholeList, volunteers);
+  });
+}
+
+// timer call for checking if reminder date is nearing, and getting new volunteers
+// if this date is close
 app.post(/[T,t]imer/, (req, res) => {
-  const DAYS_BEFORE = 7; // remind volunteers days before the target date
-  const DATA_PATH = "";
+  const DAYS_BEFORE = 14; // remind volunteers days before the target date
   const MAX_HIGH_SAMP = 0.7; // only allow 70% of options to be from most reliable
   const SENDER = "suhaskvittal@gmail.com";
 
@@ -103,12 +211,16 @@ app.post(/[T,t]imer/, (req, res) => {
   var reminderDate = date - DAYS_BEFORE;
   var remindJob = schedule.scheduleJob(reminderDate, function() {
     var database = firebase.database();
-    var data = database.ref(DATA_PATH).once('value').then((snapshot) => {
-      var volunteers = data.val()["volunteers"];
+
+    var modelVolunteers = [];
+    var volunteers = []; // all volunteers
+    var uuids = [];
+    var qvols = []; // questionable volunteers array
+    var alt = []; // alternative volunteers
+    var data = database.ref("/users").once('value').then((snapshot) => {
+      volunteers = snapshot.val()["volunteers"];
       // compute the most reliable volunteers
-      var modelVolunteers = [];
       var maxEvents = 0;
-      var uuids = []
 
       // collect UUIDS as well
       for (var i = 0; i < volunteers.length; i++) {
@@ -127,9 +239,9 @@ app.post(/[T,t]imer/, (req, res) => {
       pythonProcess.stdout.on('data', (clusters) => {
         console.log("clusters:\t" + clusters);
         // find alternatives for questionable volunteers
-        var qvols = []; // questionable volunteers array
-        var shortlist = data.val()["list"];
-        var waitlist = data.val()["waitlist"];
+
+        var shortlist = snapshot.val()["list"];
+        var waitlist = snapshot.val()["waitlist"];
         var model = modelVolunteers[Math.floor(Math.random() * modelVolunteers.length)]
         var modelCls = clusters[model];
 
@@ -138,7 +250,7 @@ app.post(/[T,t]imer/, (req, res) => {
           var index = uuids.indexOf(shortlist[i]);
           var cls = clusters[index];
           if (modelCls != cls) {
-            qvols.append(shortlist[i]);
+            qvols.append(retrieveObjectFromAttribute("UUID", shortlist[i], volunteers));
           }
         }
 
@@ -147,16 +259,15 @@ app.post(/[T,t]imer/, (req, res) => {
         // want to give volunteers with non-pristine records a chance as well
         numHigh = Math.floor(MAX_HIGH_SAMP * qvols.length);
         numLow = qvols.length - numLow;
-        alt = []
 
         for (var i = 0; i < qvols.length; i++) {
           var index = uuids.indexOf(waitlist[i]);
           var cls = clusters[index];
           if (cls != modelCls && numLow > 0) {
-            alt.push(waitlist[i]);
+            alt.push(retrieveObjectFromAttribute("UUID", waitlist[i], volunteers));
             numLow -= 1;
           } else if (cls == modelCLS && numHigh > 0) {
-            alt.push(waitList[i]);
+            alt.push(retrieveObjectFromAttribute("UUID", waitlist[i], volunteers));
             numhigh -= 1;
           }
 
@@ -164,27 +275,18 @@ app.post(/[T,t]imer/, (req, res) => {
             break;
           }
         }
-
-        /* now handle the mailing stage
-          here we should send emails to questionable volunteers and await a response.
-          a day later we should check if we received an email, and if that email
-          is a negative response. If we didn't receive anything or received a
-          negative response, then we should reach out to alternative volunteers.
-        */
-
-        for (var p in qvols) {
-          sendEmail(SENDER, p, event, "reminder");
-        }
-
-        var checkBackDate = reminderDate + 3;
-        var checkJob = schedule.scheduleJob(checkBackDate, () => {
-
-        });
       });
     });
+
+    /* now handle the mailing stage
+      here we should send emails to questionable volunteers and await a response.
+      a day later we should check if we received an email, and if that email
+      is a negative response. If we didn't receive anything or received a
+      negative response, then we should reach out to alternative volunteers.
+    */
+
+    cycleThroughParticipants(SENDER, date - DAYS_BEFORE, 5, date, qvols, alt, event["list"], volunteers);
   });
 });
-
-
 
 module.exports = app;
